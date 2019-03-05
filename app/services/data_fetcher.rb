1741ce0b368b86186078b7d84f2bbac48f1846ff
Julian Gee <julian.gee@procore.com>
2019-03-05 11:49:30 -0800

    start persisting bitmex data

--- app/services/data_fetcher.rb
+++ app/services/data_fetcher.rb
@@ -4,10 +4,24 @@ class DataFetcher
   BITMEX_API_SECRET = ENV.fetch('BITMEX_API_SECRET')
   WAIT_TIME_IN_MINUTES = 1
 
-  def self.fetch_data
-    response = fetch_binance_data
+  def self.queue_job
+    codes = fetch_data
 
-    return response.code
+    case codes
+    when codes.any? { |v| v == 429 } # rate limit reached
+      DataFetcher.delay(run_at: (WAIT_TIME_IN_MINUTES*2).minutes.from_now).queue_job
+    when codes.any? { |v| v == 418 } # ip ban for rate limit
+    else
+      DataFetcher.delay(run_at: WAIT_TIME_IN_MINUTES.minutes.from_now).queue_job
+    end
+  end
+
+  def self.fetch_data
+    response_codes = []
+    response_codes << fetch_binance_data
+    response_codes << fetch_bitmex_data
+
+    return response_codes
   end
 
   def self.fetch_binance_data
@@ -36,49 +50,38 @@ class DataFetcher
       end
     end
 
-    response
+    response.code
   end
 
   def self.fetch_bitmex_data
     expires = 1.minute.from_now.to_i.to_s
 
-    params = { 'filter' => { 'symbol' => "XBTUSD" }, 'count' => 500 }
+    latest_traded_at = Trade.bitmex.order(:traded_at).last&.traded_at || 1.minute.ago
+    params = { 'filter' => { 'symbol' => "XBTUSD", 'startTime' => latest_traded_at.strftime("%Y-%m-%d %H:%M") }, 'count' => 500 }
     headers = { "api-expires"  => expires, 'api-key' => BITMEX_API_KEY, 'api-signature' => bitmex_api_signature(expires, params) }
 
     response = HTTParty.get('https://www.bitmex.com' + bitmex_path(params), headers: headers)
 
-    # if response.code == 200
-    #   latest_traded_at = Trade.binance.order(:traded_at).last&.traded_at || 0
+    if response.code == 200
 
-    #   response.each do |obj|
-    #     traded_at = Time.at(obj["time"].to_f / 1000)
+      response.each do |obj|
+        traded_at = DateTime.parse(obj['timestamp'])
 
-    #     if traded_at > latest_traded_at
-    #       taker_side = obj["isBuyerMaker"] ? Trade::SELL : Trade::BUY
+        if traded_at > latest_traded_at
+          taker_side = obj["side"] == "Sell" ? Trade::SELL : Trade::BUY
 
-    #       Trade.create(
-    #         price: "%f" % obj["price"],
-    #         quantity: "%f" % obj["qty"],
-    #         taker_side: taker_side,
-    #         traded_at: traded_at,
-    #         source: Trade::BINANCE
-    #       )
-    #     end
-    #   end
-    # end
-    response
-  end
-
-  def self.queue_job
-    code = fetch_data
-
-    case code
-    when 429 # rate limit reached
-      DataFetcher.delay(run_at: (WAIT_TIME_IN_MINUTES*2).minutes.from_now).queue_job
-    when 418 # ip ban for rate limit
-    else
-      DataFetcher.delay(run_at: WAIT_TIME_IN_MINUTES.minutes.from_now).queue_job
+          Trade.create(
+            price: "%f" % obj["price"],
+            quantity: "%f" % obj["size"],
+            taker_side: taker_side,
+            traded_at: traded_at,
+            source: Trade::BITMEX
+          )
+        end
+      end
     end
+
+    response.code
   end
 
   def self.bitmex_path(params)
