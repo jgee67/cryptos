9f2df3705cb7c50b452c017b579db2b6e8477cb0
Julian Gee <julian.gee@procore.com>
2019-04-04 14:58:38 -0700

    self-deleting trades

--- app/services/data_fetcher.rb
+++ app/services/data_fetcher.rb
@@ -2,17 +2,25 @@ class DataFetcher
   BINANCE_API_KEY = ENV.fetch('BINANCE_API_KEY')
   BITMEX_API_KEY = ENV.fetch('BITMEX_API_KEY')
   BITMEX_API_SECRET = ENV.fetch('BITMEX_API_SECRET')
-  WAIT_TIME_IN_MINUTES = 1
+  WAIT_TIME_IN_SECONDS = 45
+  MAXIMUM_TRADE_THRESHOLD = 9_000_000
+  COUNT_TRADES_TO_PURGE = 3_000_000
 
   def self.queue_job
     codes = fetch_data
 
+    if Trade.count > MAXIMUM_TRADE_THRESHOLD
+      first_trade_id = Trade.first.id
+      upper_id_bound = first_trade_id + COUNT_TRADES_TO_PURGE
+      Trade.where(id: first_trade_id..upper_id_bound).delete_all
+    end
+
     case codes
     when codes.any? { |v| v == 429 } # rate limit reached
-      DataFetcher.delay(run_at: (WAIT_TIME_IN_MINUTES*2).minutes.from_now).queue_job
+      DataFetcher.delay(run_at: (WAIT_TIME_IN_SECONDS*2).seconds.from_now).queue_job
     when codes.any? { |v| v == 418 } # ip ban for rate limit
     else
-      DataFetcher.delay(run_at: WAIT_TIME_IN_MINUTES.minutes.from_now).queue_job
+      DataFetcher.delay(run_at: WAIT_TIME_IN_SECONDS.seconds.from_now).queue_job
     end
   end
 
